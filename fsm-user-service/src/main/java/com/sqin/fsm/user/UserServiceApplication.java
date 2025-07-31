5045d1e70895487c95aaf16c5ccd5c9e3dfb0197
sqin <sheng.qin@hotmail.com>
2025-07-31 22:47:31 +0800

    git commit -m "Initialize FSM platform with user-service module and basic setup"

---
+++ fsm-user-service/src/main/java/com/sqin/fsm/user/UserServiceApplication.java
@@ -0,0 +1,17 @@
+package com.sqin.fsm.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Author: Mr. Qin
+ * Created: 2025-07-31 22:45
+ */
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+}
